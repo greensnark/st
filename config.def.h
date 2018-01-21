2db0f11728902968f8e32fdb63240033ad7ea21d
Darshan Shaligram <scintilla@gmail.com>
2018-01-21 13:42:45 -0500

    Tighter character bounding boxes

--- config.def.h
+++ config.def.h
@@ -24,8 +24,8 @@ char *stty_args = "stty raw pass8 nl -echo -iexten -cstopb 38400";
 char *vtiden = "\033[?6c";
 
 /* Kerning / character bounding-box multipliers */
-static float cwscale = 1.0;
-static float chscale = 1.0;
+float cwscale = 0.9;
+float chscale = 1.0;
 
 /*
  * word delimiter string
