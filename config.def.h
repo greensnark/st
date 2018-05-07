23243ada0190a227d13a38d0d83ff966eabc562a
Darshan Shaligram <scintilla@gmail.com>
2018-05-07 07:45:52 -0400

    Switch to Ubuntu Mono, italics <3

--- config.def.h
+++ config.def.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-char font[] = "Fira Mono:size=9.5";
+char font[] = "Ubuntu Mono:size=11";
 int borderpx = 2;
 
 /*
